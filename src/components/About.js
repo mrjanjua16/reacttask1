41e5aa243530c7ccf41895127fd4efd1c2116b66
Mubashir Ahmad <imubashirahmad@gmail.com>
2024-05-06 11:04:04 +0000

    About styling

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,6 @@
-import speakerImg from '../components/images/'
+import speakerImg from '../components/images/speakers/featured-speaker.jpg';
+import './AboutStyle.css'
+
 
 export default function About(){
     return (
@@ -7,11 +9,31 @@ export default function About(){
             <div className="row">
                 <div className="col-lg-4">
                     <div className="image-block">
-                        <img className="img-fluid" src={}/>
+                        <img className="img-fluid" src={speakerImg}/>
                     </div>
                 </div>
                 <div className="col-lg-8">
-                <h1>About1</h1>
+                    <div className='content-block'>
+                        <h2>About The
+                            <span className='alternate'>Eventre</span>
+                        </h2>
+                        <div className='description-one'></div>
+                            <p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusm tempor incididunt ut labore dolore magna aliqua enim ad minim veniam quis nostrud exercitation ullamco.
+						</p>
+                        <div className='description-two'></div>
+                        <p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusm tempor incididunt ut labore dolore magna aliqua enim ad minim veniam quis nostrud exercitation ullamco.
+						</p>
+                        <ul className='list-inline'>
+                            <li className='list-inline-item'></li>
+                            <a href='#' className='btn-main-md1'>Buy ticket</a>
+                            <li className='list-inline-item'>
+                                <a href='#' className='btn-transparent-md1'>Read more</a>
+                            </li>
+                        </ul>
+
+                    </div>
                 </div>
 
             </div>
